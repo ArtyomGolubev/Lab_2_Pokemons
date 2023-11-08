ddd54619f4bec307dc56ab81f115d741d7f015d4
ArtyomGolubev <145132730+ArtyomGolubev@users.noreply.github.com>
2023-11-08 13:15:26 +0300

    Vibrava.java

--- src/mypokemons/Vibrava.java
+++ src/mypokemons/Vibrava.java
@@ -5,11 +5,11 @@ import mymoves.Rest;
 import ru.ifmo.se.pokemon.Pokemon;
 import ru.ifmo.se.pokemon.Type;
 
-public class Vibrava extends Pokemon {
+public class Vibrava extends Pokemon//extends Trapinch {
     public Vibrava(String name, int level) {
         super (name, level);
-        super.setType(Type.GROUND, Type.DRAGON);
+        super.setType(Type.GROUND, Type.DRAGON); // super.setType(Type.Dragon);
         super.setStats(50, 70, 50, 50, 50,70);
-        super.setMove(new Facade(), new Rest(), new Bug_Buzz());
+        super.setMove(new Facade(), new Rest(); new Bug_Buzz()); //super.setMove(new Bug_Buzz());
     }
 }
